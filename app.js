11431508e6936004dc3ff0bcb15500f1e018fcb2
jsgitter <oscar.chiaradia@gmail.com>
2021-10-27 10:46:28 -0300

    agregue ruta y controlador a catalogos lineaHogar y cuidadoPersonal

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
 const express = require("express");
-
-const path = require( "path" );
-const ProductoDetalladoRutas = require("./routes/productoDetalladoRoute.js");
-
-
 const app = express();
 
-const PORT    = 3131;
-
+const path = require( "path" );
 app.use( express.static( path.join( __dirname, "public" ) ) );
 
+const PORT    = 3131;
 app.listen( PORT, () => { console.log( `Eco HONeY corriendo en el puerto ${PORT}` ) } );
 
+
+/* Sistema de ruteo original */
 app.get( "/", (req,res) => { res.sendFile( path.resolve( __dirname, "./views/index.html" ) ) } );
-app.get( "/views/lineaHogar.html", (req,res) => { res.sendFile( path.resolve( __dirname, "./views/lineahogar.html" ) ) } );
-
-//sumando los otros get
-
-app.get( "/views/lineaCuidadoPersonal.html", (req,res) => { res.sendFile( path.resolve( __dirname, "./views/lineaCuidadoPersonal.html" ) ) } );
+/* app.get( "/views/lineaCuidadoPersonal.html",  (req,res) => { res.sendFile( path.resolve( __dirname, "../views/lineaCuidadoPersonal.html" ) ) } ); */
+/* app.get( "//linviewseaHogar.html", (req,res) => { res.sendFile( path.resolve( __dirname, "./views/lineahogar.html" ) ) } ); */
 app.get( "/views/lineaProductoDeLasAbejas.html", (req,res) => { res.sendFile( path.resolve( __dirname, "./views/lineaProductoDeLasAbejas.html" ) ) } );
 app.get( "/views/login.html", (req,res) => { res.sendFile( path.resolve( __dirname, "./views/login.html" ) ) } );
 app.get( "/views/registro.html", (req,res) => { res.sendFile( path.resolve( __dirname, "./views/registro.html" ) ) } );
 app.get( "/views/carrito.html", (req,res) => { res.sendFile( path.resolve( __dirname, "./views/carrito.html" ) ) } );
-app.use( "/views/productoDetallado.html", ProductoDetalladoRutas);
+/* ----------------- FIN ------------------ */
+
+
+/* MVC - Requires al sistema de ruteo */
+const ProductoDetalladoRutas = require("./routes/productoDetalladoRoute.js");
+const cuidadoPersonalRutas = require( "./routes/cuidadoPersonalRoute.js");
+const lineaHogarRutas = require( './routes/lineaHogarRoute.js' );
+/* ----------------- FIN ------------------ */
+
+/* MVC - Ruteo por Controladores */
+app.use( "/views/productoDetallado.html", ProductoDetalladoRutas );
+app.use( "/views/lineaCuidadoPersonal.html", cuidadoPersonalRutas );
+app.use( "/views/lineaHogar.html", lineaHogarRutas );
+/* ----------------- FIN ------------------ */
