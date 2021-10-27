6f363d8193cc5b6d52d2172c28afdc49b991a0bf
heidymarg <heidymarg5@gmail.com>
2021-10-27 14:50:07 -0300

    controller,route Linea Producto De Las Abejas

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.listen( PORT, () => { console.log( `Eco HONeY corriendo en el puerto ${PORT}
 app.get( "/", (req,res) => { res.sendFile( path.resolve( __dirname, "./views/index.html" ) ) } );
 /* app.get( "/views/lineaCuidadoPersonal.html",  (req,res) => { res.sendFile( path.resolve( __dirname, "../views/lineaCuidadoPersonal.html" ) ) } ); */
 /* app.get( "//linviewseaHogar.html", (req,res) => { res.sendFile( path.resolve( __dirname, "./views/lineahogar.html" ) ) } ); */
-app.get( "/views/lineaProductoDeLasAbejas.html", (req,res) => { res.sendFile( path.resolve( __dirname, "./views/lineaProductoDeLasAbejas.html" ) ) } );
+/*app.get( "/views/lineaProductoDeLasAbejas.html", (req,res) => { res.sendFile( path.resolve( __dirname, "./views/lineaProductoDeLasAbejas.html" ) ) } );*/
 app.get( "/views/login.html", (req,res) => { res.sendFile( path.resolve( __dirname, "./views/login.html" ) ) } );
 app.get( "/views/registro.html", (req,res) => { res.sendFile( path.resolve( __dirname, "./views/registro.html" ) ) } );
 app.get( "/views/carrito.html", (req,res) => { res.sendFile( path.resolve( __dirname, "./views/carrito.html" ) ) } );
@@ -23,10 +23,12 @@ app.get( "/views/carrito.html", (req,res) => { res.sendFile( path.resolve( __dir
 const ProductoDetalladoRutas = require("./routes/productoDetalladoRoute.js");
 const cuidadoPersonalRutas = require( "./routes/cuidadoPersonalRoute.js");
 const lineaHogarRutas = require( './routes/lineaHogarRoute.js' );
+const lineaProductoDeLasAbejasRutas = require('./routes/lineaProductoDeLasAbejasRoute.js');
 /* ----------------- FIN ------------------ */
 
 /* MVC - Ruteo por Controladores */
 app.use( "/views/productoDetallado.html", ProductoDetalladoRutas );
 app.use( "/views/lineaCuidadoPersonal.html", cuidadoPersonalRutas );
 app.use( "/views/lineaHogar.html", lineaHogarRutas );
+app.use("/views/lineaProductoDeLasAbejas.html", lineaProductoDeLasAbejasRutas);
 /* ----------------- FIN ------------------ */
