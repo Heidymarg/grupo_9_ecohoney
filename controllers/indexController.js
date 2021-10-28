ee99b04971c7f93c7f3c2374229e74b87c37436b
Yariela Silva <yarielasilva17@gmail.com>
2021-10-28 10:58:00 -0300

    agregando indexController

---
+++ controllers/indexController.js
@@ -0,0 +1,7 @@
+const path = require( 'path');
+
+const indexController = {
+index: (req,res) => { res.sendFile( path.resolve( __dirname, "../views/index.html" ) ) },
+};
+
+module.exports = indexController;
