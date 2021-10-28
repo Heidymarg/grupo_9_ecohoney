4526ea09b97d16ef6939510b157f66a9a311d367
Yariela Silva <yarielasilva17@gmail.com>
2021-10-28 10:57:07 -0300

    cambio de app.js agregando ruta de index

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 const path = require( "path" );
+
 app.use( express.static( path.join( __dirname, "public" ) ) );
 
 const PORT    = 3131;
@@ -9,7 +10,7 @@ app.listen( PORT, () => { console.log( `Eco HONeY corriendo en el puerto ${PORT}
 
 
 /* Sistema de ruteo original */
-app.get( "/", (req,res) => { res.sendFile( path.resolve( __dirname, "./views/index.html" ) ) } );
+/*app.get( "/", (req,res) => { res.sendFile( path.resolve( __dirname, "./views/index.html" ) ) } );*/
 /* app.get( "/views/lineaCuidadoPersonal.html",  (req,res) => { res.sendFile( path.resolve( __dirname, "../views/lineaCuidadoPersonal.html" ) ) } ); */
 /* app.get( "//linviewseaHogar.html", (req,res) => { res.sendFile( path.resolve( __dirname, "./views/lineahogar.html" ) ) } ); */
 /*app.get( "/views/lineaProductoDeLasAbejas.html", (req,res) => { res.sendFile( path.resolve( __dirname, "./views/lineaProductoDeLasAbejas.html" ) ) } );*/
@@ -20,6 +21,7 @@ app.get( "/views/carrito.html", (req,res) => { res.sendFile( path.resolve( __dir
 
 
 /* MVC - Requires al sistema de ruteo */
+const indexRutas= require("./routes/indexRoute.js");
 const ProductoDetalladoRutas = require("./routes/productoDetalladoRoute.js");
 const cuidadoPersonalRutas = require( "./routes/cuidadoPersonalRoute.js");
 const lineaHogarRutas = require( './routes/lineaHogarRoute.js' );
@@ -27,6 +29,7 @@ const lineaProductoDeLasAbejasRutas = require('./routes/lineaProductoDeLasAbejas
 /* ----------------- FIN ------------------ */
 
 /* MVC - Ruteo por Controladores */
+app.use( "/", indexRutas );
 app.use( "/views/productoDetallado.html", ProductoDetalladoRutas );
 app.use( "/views/lineaCuidadoPersonal.html", cuidadoPersonalRutas );
 app.use( "/views/lineaHogar.html", lineaHogarRutas );
