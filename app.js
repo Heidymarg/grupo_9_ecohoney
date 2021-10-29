ba6217189df500570952caa1261c64abc63bf5df
jsgitter <oscar.chiaradia@gmail.com>
2021-10-29 19:46:35 -0300

    head.ejs header.ejs footer.ejs

--- app.js
+++ app.js
@@ -4,12 +4,27 @@ const app = express();
 const path = require( "path" );
 app.use( express.static( path.join( __dirname, "public" ) ) );
 
+app.set('view engine', 'ejs'); 
+const homeController = require( 'indexController.js');
+
+/* implementación de partials 
+let ejs = require('ejs');
+
+app.set('views', './head');
+app.set( 'views', './header' );
+app.set('views', './footer');
+
+
+ **** Fin partials ******   */
+
+
 const PORT    = 3131;
+
 app.listen( PORT, () => { console.log( `Eco HONeY corriendo en el puerto ${PORT}` ) } );
 
 
 /* Sistema de ruteo original */
-app.get( "/", (req,res) => { res.sendFile( path.resolve( __dirname, "./views/index.html" ) ) } );
+/* app.get( "/", (req,res) => { res.sendFile( path.resolve( __dirname, "./views/index.html" ) ) } ); */
 /* app.get( "/views/lineaCuidadoPersonal.html",  (req,res) => { res.sendFile( path.resolve( __dirname, "../views/lineaCuidadoPersonal.html" ) ) } ); */
 /* app.get( "//linviewseaHogar.html", (req,res) => { res.sendFile( path.resolve( __dirname, "./views/lineahogar.html" ) ) } ); */
 app.get( "/views/lineaProductoDeLasAbejas.html", (req,res) => { res.sendFile( path.resolve( __dirname, "./views/lineaProductoDeLasAbejas.html" ) ) } );
@@ -30,3 +45,4 @@ app.use( "/views/productoDetallado.html", ProductoDetalladoRutas );
 app.use( "/views/lineaCuidadoPersonal.html", cuidadoPersonalRutas );
 app.use( "/views/lineaHogar.html", lineaHogarRutas );
 /* ----------------- FIN ------------------ */
+
