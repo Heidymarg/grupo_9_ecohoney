f61146569378320cfe02d136c87596a1fe6fd1a1
jsgitter <oscar.chiaradia@gmail.com>
2021-10-31 15:52:37 -0300

    agregue linea 4 en app.js y link a inicio de productoDelas Abejas en index-html

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const app = express();
 
 const path = require( "path" );
 
-app.use( express.static( path.join( __dirname, "public" ) ) );
-
+app.set('views', __dirname + '/views');
 
 app.set('view engine', 'ejs'); 
+
+app.use( express.static( path.join( __dirname, "public" ) ) );
 
 const PORT    = 3131;
 
