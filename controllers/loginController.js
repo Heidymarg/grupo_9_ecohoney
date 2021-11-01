e2cabc82f75360180a834f5809db9b440ba8d194
Noheliak <noheliakrodriguez@gmail.com>
2021-11-01 20:19:40 -0300

    vista dinamica de la linea de cuidado personal

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -5,11 +5,7 @@ login: (req,res) => { res.sendFile( path.resolve( __dirname, "../views/login.htm
 };
 */
 const loginController = {
-<<<<<<< HEAD
-    login:(req,res) => { res.render(( 'views', 'login' ) )},  
-=======
     login:(req,res) => { res.render( path.resolve( 'views/users', 'login' ) ) },  
->>>>>>> a627a33d2305b0f1cfc2c02ed1ed651966c79fdc
     
 
 };
--- routes/registroRoute.js
+++ routes/registroRoute.js
@@ -4,12 +4,8 @@ const router = express.Router();
 
  const registroController= require('../controllers/registroController.js');
 
-<<<<<<< HEAD
-router.get('/registro', registroController.registro);
-=======
 //router.get('/', registro.registro);
-router.get('/registro', registro.registro);
->>>>>>> a627a33d2305b0f1cfc2c02ed1ed651966c79fdc
+router.get('/registro', registroController.registro);
 
 module.exports = router;
 
