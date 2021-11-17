6033222066c378d3324aa2aca3c0e33aa63e4777
jsgitter <oscar.chiaradia@gmail.com>
2021-11-17 00:33:59 -0300

    sprint4

--- controllers/formularioCargaProductoController.js
+++ controllers/formularioCargaProductoController.js
@@ -1,8 +1,33 @@
 const path = require( 'path');
+const file = require( 'fs');
 
+var productos = file.readFileSync( path.join(__dirname, '../', '/data/listadoProductosAbejas.json'), 'utf-8')
+var productosJson = JSON.parse( productos );
 
 const formularioCargaProductoController = {
     /* Con MVC + EJS */
     inicio: (req,res) => { res.render( ( 'views', 'formularioCargaProducto' ) ) },
-    };
+
+    agregar: ( req,res) => {
+        
+        if ( req.body.desc != null && req.body.DescDeta !=null && req.body.linea != null && req.body.precio != null ) {
+            let prod = { idPrd: null, desc: null, DescDeta: null, linea: null, precio: null, bonif: null, foto: null };
+            prod.idPrd = productosJson.length + 1;
+            prod.desc = req.body.desc; 
+            prod.DescDeta = req.body.DescDeta; 
+            prod.linea = req.body.linea; 
+            prod.precio = req.body.precio; 
+            prod.bonif = req.body.bonif; 
+            prod.foto = req.body.foto;
+        
+            productosJson.push( prod );
+            file.writeFileSync(path.join(__dirname, '../', '/data/listadoProductosAbejas.json'), JSON.stringify(productosJson));
+
+            res.redirect('back');
+        }
+        
+    }
+
+};
+  
 module.exports = formularioCargaProductoController;
--- routes/formularioCargaProductoRoute.js
+++ routes/formularioCargaProductoRoute.js
@@ -2,8 +2,11 @@ const express = require( 'express' );
 
 const router = express.Router();
 
+
+
 const formularioCargaProductoController = require( '../controllers/formularioCargaProductoController.js' );
 
 router.get('/', formularioCargaProductoController.inicio);
+
 
 module.exports = router;
